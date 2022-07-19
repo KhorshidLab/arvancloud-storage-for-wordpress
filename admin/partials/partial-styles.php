86a6d00b09b669802fc5bb5242d420ebbf8055c9
armanb <armanborkhani@gmail.com>
2022-07-19 13:55:40 +0430

    feat: Add primay button

---
+++ admin/partials/partial-styles.php
@@ -0,0 +1,5 @@
+<div class="obs-content-wrapper">
+  <button class="obs-btn-primary">بعدی</button>
+  <button class="obs-btn-primary disabled">بعدی</button>
+  <button class="obs-btn-primary loading">بعدی</button>
+</div>
