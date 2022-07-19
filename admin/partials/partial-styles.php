97d9b679adc3c9d35e8f022bf99ff0f865986af4
armanb <armanborkhani@gmail.com>
2022-07-19 14:57:56 +0430

    feat: Add secondary outline button

--- admin/partials/partial-styles.php
+++ admin/partials/partial-styles.php
@@ -8,3 +8,8 @@
   <button class="obs-btn-primary-outline disabled">انصراف</button>
   <button class="obs-btn-primary-outline loading">انصراف</button>
 </div>
+<div class="obs-content-wrapper">
+  <button class="obs-btn-secondary-outline">انصراف</button>
+  <button class="obs-btn-secondary-outline disabled">انصراف</button>
+  <button class="obs-btn-secondary-outline loading">انصراف</button>
+</div>
