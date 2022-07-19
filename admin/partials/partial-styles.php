399b2462a1ada10b8b19033eaa1cec65f92cf93d
armanb <armanborkhani@gmail.com>
2022-07-19 15:36:53 +0430

    feat: Add bucket button

--- admin/partials/partial-styles.php
+++ admin/partials/partial-styles.php
@@ -18,3 +18,26 @@
   <button class="obs-btn-danger disabled">حذف فایل</button>
   <button class="obs-btn-danger loading">حذف فایل</button>
 </div>
+<div class="obs-content-wrapper">
+  <button class="obs-btn-add-bucket">
+    <svg class="icon" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M8 3.3335V12.6668" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+      <path d="M3.33325 8H12.6666" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
+    ساخت صندوقچه
+  </button>
+  <button class="obs-btn-add-bucket disabled">
+    <svg class="icon" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M8 3.3335V12.6668" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+      <path d="M3.33325 8H12.6666" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
+    ساخت صندوقچه
+  </button>
+  <button class="obs-btn-add-bucket loading">
+    <svg class="icon" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M8 3.3335V12.6668" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+      <path d="M3.33325 8H12.6666" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
+    ساخت صندوقچه
+  </button>
+</div>
