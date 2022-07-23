f20ae3d2ff5de99f455f5bfd14eb9524ee905b77
armanb <armanborkhani@gmail.com>
2022-07-23 11:03:39 +0430

    feat: Add alert modal

--- admin/partials/partial-styles.php
+++ admin/partials/partial-styles.php
@@ -148,3 +148,28 @@
     </ul>
   </footer>
 </div>
+<div class="obs-content-wrapper">
+  <div class="obs-modal obs-modal-alert">
+    <div class="obs-modal-title">هشدار</div>
+    <figure class="obs-modal-figure">
+      <svg class="icon" width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M20.5796 7.7209L3.63955 36.0009C3.29029 36.6057 3.10549 37.2915 3.10353 37.9899C3.10158 38.6884 3.28254 39.3752 3.62841 39.9819C3.97428 40.5887 4.473 41.0944 5.07497 41.4486C5.67693 41.8028 6.36115 41.9932 7.05955 42.0009H40.9396C41.638 41.9932 42.3222 41.8028 42.9241 41.4486C43.5261 41.0944 44.0248 40.5887 44.3707 39.9819C44.7166 39.3752 44.8975 38.6884 44.8956 37.9899C44.8936 37.2915 44.7088 36.6057 44.3596 36.0009L27.4196 7.7209C27.063 7.13311 26.561 6.64714 25.9619 6.30987C25.3629 5.97259 24.687 5.79541 23.9996 5.79541C23.3121 5.79541 22.6362 5.97259 22.0372 6.30987C21.4381 6.64714 20.9361 7.13311 20.5796 7.7209V7.7209Z" stroke="#EE5353" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
+        <path d="M24 18V26" stroke="#EE5353" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
+        <path d="M24 34.0002H24.02" stroke="#EE5353" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
+      </svg>
+    </figure>
+    <div class="obs-modal-desc">حذف کردن تمامی فایل های محلی</div>
+    <div class="obs-modal-desc-small">با  این عمل تمامی فایل های محلی شما حذف خواهد شد.</div>
+    <div class="obs-modal-confirm">
+      <div class="obs-form-check">
+        <input class="obs-input" type="checkbox" name="exampleRadios" id="modal" value="option1">
+        <div class="obs-custom-input"></div>
+      </div>
+      <label for="modal">از حذف گروهی فایل‌ها اطمینان دارم</label>
+    </div>
+    <div class="obs-modal-actions">
+      <button class="obs-btn-secondary-outline">انصراف</button>
+      <button class="obs-btn-danger">حذف فایل</button>
+    </div>
+  </div>
+</div>
